6914cc649a9332a47f9452f4034e4118e1b4e866
nadavatar <nadavatar64@gmail.com>
2019-01-19 16:09:45 +0200

    asadf

--- src/firebase.js
+++ src/firebase.js
@@ -1,43 +1,61 @@
 
-      // Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyCbQJDzonsTfgUz1IEwDxYSvfxMdw6PHGE",
-        authDomain: "mikan-dev-d2b71.firebaseapp.com",
-        databaseURL: "https://mikan-dev-d2b71.firebaseio.com",
-        projectId: "mikan-dev-d2b71",
-        storageBucket: "mikan-dev-d2b71.appspot.com",
-        messagingSenderId: "789702102796"
-      };
-      
-      firebase.initializeApp(config);
-      
-      var db = firebase.firestore();
-      const settings = {timestampsInSnapshots: true};
-      db.settings(settings);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCbQJDzonsTfgUz1IEwDxYSvfxMdw6PHGE",
+  authDomain: "mikan-dev-d2b71.firebaseapp.com",
+  databaseURL: "https://mikan-dev-d2b71.firebaseio.com",
+  projectId: "mikan-dev-d2b71",
+  storageBucket: "mikan-dev-d2b71.appspot.com",
+  messagingSenderId: "789702102796"
+};
 
-     
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
+
+
+
+function addMember(name, status) {
+  return new Promise((resolve, reject) => {
+    db.collection("members").add({
+      name,
+      status, 
+    })
+    .then(function(docRef) {
+      resolve("Document written with ID: " + docRef.id);
+      members = getMembers();
+      renderMembers(members);
+    })
+    .catch(function(error) {
+      reject("Error adding document: " + error);
+    });
+  });
+  
+}
+
+function getAllMembers() {
+  return new Promise((resolve, reject) => {
+    // TODO get all members
     
-
-    function getAllMembers() {
-      return new Promise((resolve, reject) => {
-        // TODO get all members
-
-        db.collection("members").get()
-        .then((querySnapshot) => {
-          const members = [];
-          querySnapshot.forEach((doc) => {
-              // console.log(`${doc.id} => ${doc.data().status}`);
-              member = {
-                name: doc.data().name,
-                status: doc.data().status
-              }
-              members.push(member);
-          });
-           resolve(members);
-
-      })
-      .catch(error => reject(error));
-
-        // resolve(members);
-      })
-    }
+    db.collection("members").get()
+    .then((querySnapshot) => {
+      const members = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data().status}`);
+        member = {
+          name: doc.data().name,
+          status: doc.data().status
+        }
+        members.push(member);
+      });
+      resolve(members);
+      
+    })
+    .catch(error => reject(error));
+    
+    // resolve(members);
+  })
+}
