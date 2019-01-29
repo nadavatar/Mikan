4ac11c75ac02136ab0439239237ae525cd77e9b2
nadavatar <nadavatar64@gmail.com>
2019-01-29 22:00:49 +0200

    asdf

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ let members = [];
 
 const memberHtmlTemplate = `
                 <tr>
-                <td id="name">{{namePlaceholder}}</td>
+                <td>{{namePlaceholder}}</td>
                 <td>{{statusPlaceHolder}}</td>
                 <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
                 <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button onclick="removeMember({{namePlaceholder}})" class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
